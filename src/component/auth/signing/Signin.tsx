2efc7e4d02868a8d2df7bcda0b91dc1894cf8592
Nelson <neldecas12@gmail.com>
2021-12-18 15:34:18 -0500

    gg

--- src/component/auth/signing/Signin.tsx
+++ src/component/auth/signing/Signin.tsx
@@ -1,13 +1,23 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Context } from "../../../provider/context";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../custom-hooks/Auth/auth";
+import { _Link as Link } from "../../UI/Link";
+import "./signin.css";
 
 const Signin = () => {
     const [user, setuser] = useState({ email: "", password: "" });
     const { dispatch } = useContext(Context);
     const history = useHistory();
+
+    useEffect(() => {
+        const element = document.querySelector(".content-separate");
+        element?.classList.toggle("content-separate_signin");
+        return () => {
+            element?.classList.toggle("content-separate_signin");
+        };
+    }, []);
 
     const handlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setuser({ ...user, [e.target.name]: e.target.value });
@@ -24,11 +34,32 @@ const Signin = () => {
     };
 
     return (
-        <div>
-            <div>
+        <div className="content-signin">
+            <div className="content-signin_header">
+                <h1>Signin</h1>
+            </div>
+            <div className="content-signin_body">
                 <input type="text" onChange={handlerInput} name="email" />
                 <input type="password" onChange={handlerInput} name="password" />
-                <button onClick={handlerSubmit}>Send</button>
+                <button onClick={handlerSubmit}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-send-fill util-space"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89.471-1.178-1.178.471L5.93 9.363l.338.215a.5.5 0 0 1 .154.154l.215.338 7.494-7.494Z"
+                        />
+                    </svg>
+                    Movies
+                </button>
+            </div>
+            <div className="content-signin_expands">
+                <Link url="/signup"> No tienes Cuenta?</Link>
             </div>
         </div>
     );
--- src/component/auth/signup/Signup.tsx
+++ src/component/auth/signup/Signup.tsx
@@ -1,9 +1,19 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import { Context } from "../../../provider/context";
+import { _Link as Link } from "../../UI/Link";
+import "./signup.css";
 
 const Signup = () => {
     const [user, setuser] = useState({ name: "", lastname: "", age: 0, email: "", password: "" });
     const { dispatch } = useContext(Context);
+
+    useEffect(() => {
+        const element = document.querySelector(".content-separate");
+        element?.classList.toggle("content-separate_signup");
+        return () => {
+            element?.classList.toggle("content-separate_signup");
+        };
+    }, [])
 
     const handlerInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setuser({ ...user, [e.target.name]: e.target.value })
@@ -14,14 +24,20 @@ const Signup = () => {
     };
 
     return (
-        <div>
-            <div>
+        <div className="content-signup" >
+            <div className="content-signup_header">
+                <h1>Signup</h1>
+            </div>
+            <div className="content-signup_body" >
                 <input type="text" onChange={handlerInput} name="name" />
                 <input type="text" onChange={handlerInput} name="lastname" />
                 <input type="number" onChange={handlerInput} name="age" />
                 <input type="text" onChange={handlerInput} name="email" />
                 <input type="password" onChange={handlerInput} name="password" />
                 <button onClick={handlerSubmit} >Send</button>
+            </div>
+            <div className="content-signup_expands" >
+                <Link url="/login"> Ya tienes Cuenta?</Link>
             </div>
         </div>
     );
